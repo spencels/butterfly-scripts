29226f60e8a4c003b15d5c033b7591599b2c8276
Spencer Stephens <spencer.leo.stephens@gmail.com>
2021-03-11 01:09:20 -0800

    Set ansible inventory via env variable.

--- env.sh
+++ env.sh
@@ -2,9 +2,20 @@
 #   source ./env.sh  # dev config
 
 function main() {
-  local env="${1:-dev}"
+  local env="$1"
   # Set vars.
   local script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+  case $env in
+  prod)
+    # Clear so the ansible.cfg one is used.
+    export ANSIBLE_INVENTORY="$script_dir/ansible/inventory.yml"
+    ;;
+  *)
+    export ANSIBLE_INVENTORY="$HOME/buttsdev/inventory.yml"
+    ;;
+  esac
+  echo "ANSIBLE_INVENTORY=$ANSIBLE_INVENTORY"
+
   export ANSIBLE_CONFIG="$script_dir/ansible/ansible.cfg"
   echo "ANSIBLE_CONFIG=$ANSIBLE_CONFIG"
 }
