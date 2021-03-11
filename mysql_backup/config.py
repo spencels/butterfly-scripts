b3c98997f57ce2165ed5f423472226a273130ae5
Spencer Stephens <spencer.leo.stephens@gmail.com>
2021-03-11 01:05:17 -0800

    gitignore .s3cfg and remove s3cfg

--- mysql_backup/config.py
+++
@@ -1,4 +0,0 @@
-mysql_user = 'blah'
-mysql_password = 'blah'
-s3_hostname = 'ewr1.vultrobjects.com'
-s3_secret_key = 'blah'
--- mysql_backup/run-mysqldump.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Usage: run-mysqldump.sh username password outputfile
-mysqldump --user="$1" --password="$2" --all_databases | gzip >"$3"
-exit $?
